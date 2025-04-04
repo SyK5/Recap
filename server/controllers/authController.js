ff286f96d555ee20e3e213a3d40b5c7e8b3f2f83
SyK5 <Alpay.Sahin@outlook.de>
2025-04-04 18:52:06 +0200

    kurz den auth controll gefixt mache später mit asynchandler zsm für try catch und error handler.

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,7 +1,9 @@
+import asyncHandler from "../middleware/asyncHandler";
 import CheckAuth from "../middleware/auth";
 
-app.get("/api/auth", CheckAuth, (req, res) => {
-  console.log(`Auth verifiziert: ${req.user}`);
 
-  res.status(200).json({ user: req.user });
+const Authenfication = CheckAuth(async(req, res, next) => {
+    console.log(`Auth verifiziert: ${req.user}`);
+
+    res.status(200).json({ user: req.user });
 });
